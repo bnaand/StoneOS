28b5a6838e50e29afa442af2f5384487b8eb9e42
bnaand <106130584+bnaand@users.noreply.github.com>
2024-04-24 21:53:30 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #增加主题
-echo "CONFIG_PACKAGE_luci-theme-$OpenWrt_THEME=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-$OpenWrt_THEME-config=y" >> .config
+# echo "CONFIG_PACKAGE_luci-theme-$OpenWrt_THEME=y" >> .config
+# echo "CONFIG_PACKAGE_luci-app-$OpenWrt_THEME-config=y" >> .config
 
 #根据源码来修改
 if [[ $OpenWrt_URL != *"lede"* ]] ; then
